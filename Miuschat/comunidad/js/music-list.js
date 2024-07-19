859d9520089e6159564ae755bdb94306b67d2d9f
Maicol355 <mariowasson39@gmail.com>
2024-07-18 19:43:20 -0600

    Add files via upload

---
+++ Miuschat/comunidad/js/music-list.js
@@ -0,0 +1,80 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Stuck in the sound",
+    artist: "let's go",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Skip the use",
+    artist: "Namelees World",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Summit",
+    artist: "Skrillex",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Make it Bun Bem",
+    artist: "Skrillex & Damian",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Bangorang",
+    artist: "Sirah",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Animals",
+    artist: "Martin Garrix",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Bad",
+    artist: "Vassy",
+    img: "music-13",
+    src: "music-13"
+  },
+];
